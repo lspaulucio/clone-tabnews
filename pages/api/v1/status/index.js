f7ac69f3c1386092b405a9f69b1da7d816ff2931
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2024-04-27 18:01:34 -0300

    feat: Finishing `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  response.status(200).json({ message: "Test status endpoint" });
+  const updatedAt = new Date().toISOString();
+
+  const pgVersionResult = await database.query("SHOW server_version;");
+  const pgVersionValue = pgVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const activeConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const activeConnectionsValue = activeConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: activeConnectionsValue,
+        version: pgVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
