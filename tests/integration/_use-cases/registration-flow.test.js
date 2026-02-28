4de81b50b5a6ff2ac8870a8a68421b15a41634e9
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2026-02-28 09:27:16 -0300

    feat: add default feature `read:activation_token` when creating `user`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -32,7 +32,7 @@ describe("Use case: Registration Flow (all successful)", () => {
       id: createdUserResponseBody.id,
       username: "RegistrationFlow",
       email: "registration.flow@gmail.com",
-      features: [],
+      features: ["read:activation_token"],
       password: createdUserResponseBody.password,
       created_at: createdUserResponseBody.created_at,
       updated_at: createdUserResponseBody.updated_at,
