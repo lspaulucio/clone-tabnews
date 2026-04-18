762d85f23037f782bff9292f5913dcbdce04ebe7
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2026-04-18 17:49:28 -0300

    refactor: improve error login in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -19,13 +19,14 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
     this.name = "InternalServerError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -33,6 +34,7 @@ export class ServiceError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
+      context: this.context,
       status_code: this.statusCode,
     };
   }
