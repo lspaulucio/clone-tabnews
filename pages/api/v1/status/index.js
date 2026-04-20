d86baa028e4b6d229f5f5ce8d63309ae62ff4722
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2026-04-20 15:09:22 -0300

    refactor: explicitly return in `/api/v1/status` `GET` handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -44,5 +44,5 @@ async function getHandler(request, response) {
     statusObject,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
